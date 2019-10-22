2f00ac89dac3d2141b5b705ac8e339c383c4abf7
PK <pk@pknw1.co.uk>
2019-10-22 15:47:25 +0200

    Tue 22 Oct 15:47:25 CEST 2019

---
+++ buildall.sh
@@ -0,0 +1,9 @@
+PWD=$(pwd)
+find $PWD -type f -mindepth 2 -maxdepth 2 -iname Dockerfile | sed 's/\/Dockerfile//' |while read -r FILE; do
+cd $FILE
+TAG=$(echo $FILE | awk -F/ '{print $NF}')
+docker build -t pknw1/$TAG .
+docker push pknw1/$TAG
+cd $PWD
+
+done
